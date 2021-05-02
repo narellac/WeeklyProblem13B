f5f35d4f740d0f8e7334c6ca87ad3a94e2431425
Narella Calvente <narellacalvente@gmail.com>
2021-05-02 10:44:22 -0300

    BQA-13: creation of code

--- example2.js
+++ example2.js
@@ -1,16 +1,17 @@
-describe('Google Page', () => {
-
-  beforeAll('open browser', () => {
-    return browser.url('https://www.google.com/');
+describe('Search Wikipedia page', () => {
+  it('Open Google page', () => {
+      browser.url('https://www.google.com/')
   });
 
-  it('can handle commands synchronously', () => {
-    let value = $('#lb');
-    expect(value).toExist();
+  it('Write Wikipedia in text field', () => {
+      let input = $('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input');
+      input.addValue('Wikipedia');
+      browser.keys('Enter')
   })
 
-  it('title', () => {
-    browser.pause(2000);
-    expect(browser.getTitle()).toMatch('Google');
+  it('Verify that the URL obtained matches the one described', () => {
+      let wikipedia = $('#rso > div:nth-child(1) > div:nth-child(1) > div > div.tF2Cxc > div.yuRUbf > a');
+      wikipedia.click();
+      expect(browser).toHaveUrl('https://es.wikipedia.org/wiki/Wikipedia:Portada');
   })
 });
